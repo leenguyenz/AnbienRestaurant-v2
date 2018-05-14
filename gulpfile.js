abeb3cef442640a9502079d8627b9147295668d1
Nguyen Hoang Anh <anhnh@goquo.com>
2018-05-14 17:47:16 +0700

    day2
    day2

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 // global require
+"use strict";
+
+//variable task
 var gulp = require('gulp'),
 	path = require('path'),
 	pug = require('gulp-pug'),
 	data = require('gulp-data');
+
+//Directories path
+var path = {
+	dist: './dist/',
+	data: './app/_data'
+}
 
 gulp.task('pug', function () {
 	return gulp.src('./app/*.pug')
